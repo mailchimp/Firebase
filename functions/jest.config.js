cd7b1ceb8679499bf7734e2943ff5905dc14efe8
barticus <bart.breen@twobulls.com>
2022-08-25 17:40:25 +1000

    Runtime and dependencies bump (#45)
    * Bump node runtimes for functions up to a current maintained release (node 16).
    * Split out unit tests to separate files, split out shared code.
    * Package updates and audit

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
-	testRegex: '/tests/.*|(\\.|/)(test|spec)\\.(jsx?|tsx?)$',
+	testMatch: ['**/tests/*.test.js'],
 	testPathIgnorePatterns: ['node_modules/'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	testEnvironment: 'node',
