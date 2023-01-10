6925cb86d168d67383245de08aba6e224effc83f
Bart Breen <bart.breen@twobulls.com>
2023-01-10 12:17:25 +1100

    Added new configuration spec to validation schemas

--- functions/validation.js
+++ functions/validation.js
@@ -1,11 +1,29 @@
 const Validator = require("jsonschema").Validator;
 const v = new Validator();
 
+const multidimensionalSelectorSchema = {
+  id: "/MultiDimensionalSelector",
+  type: "object",
+  properties: {
+    documentPath: { type: "string" },
+    valueSelector: { type: "string" },
+  },
+  required: ["documentPath"],
+};
+
 const tagConfigSchema = {
   id: "/TagConfig",
   type: "object",
   properties: {
-    memberTags: { type: "array", items: { type: "string" } },
+    memberTags: {
+      type: "array",
+      items: {
+        oneOf: [
+          { type: "string" },
+          { $ref: multidimensionalSelectorSchema.id },
+        ],
+      },
+    },
     subscriberEmail: { type: "string" },
   },
   required: ["memberTags", "subscriberEmail"],
@@ -15,7 +33,15 @@ const eventsConfigSchema = {
   id: "/EventsConfig",
   type: "object",
   properties: {
-    memberEvents: { type: "array", items: { type: "string" } },
+    memberEvents: {
+      type: "array",
+      items: {
+        oneOf: [
+          { type: "string" },
+          { $ref: multidimensionalSelectorSchema.id },
+        ],
+      },
+    },
     subscriberEmail: { type: "string" },
   },
   required: ["memberEvents", "subscriberEmail"],
@@ -25,11 +51,16 @@ const mergeFieldsConfigSchema = {
   id: "/MergeFieldsConfig",
   type: "object",
   properties: {
-    mergeFields: { type: "object", additionalProperties: { type: "string" } },
+    mergeFields: {
+      type: "object",
+      additionalProperties: { type: "string" }
+    },
     subscriberEmail: { type: "string" },
   },
   required: ["mergeFields", "subscriberEmail"],
 };
+
+v.addSchema(multidimensionalSelectorSchema, multidimensionalSelectorSchema.id);
 
 exports.validateTagConfig = (tagConfig) =>
   v.validate(tagConfig, tagConfigSchema);
