1185da1456f654d6690d36e6e5dffce71d9dab57
Bart Breen <bart.breen@twobulls.com>
2023-07-11 16:02:30 +1000

    Added linter config and updated packages.

---
+++ functions/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
